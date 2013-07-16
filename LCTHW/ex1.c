8b9ff4250948e3376f9b63fa054acc1a1c5ee3fd
Daniel Na <daniel.na@gmail.com>
2013-07-16 11:27:30 -0400

    Add other files until now

---
+++ LCTHW/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("Hi world.");
+    puts("Yo world.");
+    puts("Sup world.");
+
+    return 0;
+}
---
+++ LCTHW/ex6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 12345.35343;
+    char init = 'A';
+    char first_name[] = "Fred";
+    char last_name[] = "Flintstone";
+    printf(" %s, %s", first_name, last_name);
+    printf("\n");
+    printf("   ");
+}
